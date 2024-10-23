bb1cf9e66e4b0704ca25bf55902ccb6caca04907
Alexey Kontsevenko <80715798+kirieshki24@users.noreply.github.com>
2024-10-23 19:16:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ import llama_model
 import subprocess
 
 def main():
-    subprocess.run("args",capture_output=True, text=True)
+    # Run the command to install the necessary packages
+    command = [
+        "sudo", "apt-get", "install", "-y",
+        "libglib2.0-0=2.50.3-2",
+        "libnss3=2:3.26.2-1.1+deb9u1",
+        "libgconf-2-4=3.2.6-4+b1",
+        "libfontconfig1=2.11.0-6.7+b1"
+    ]
+    
+    # Execute the command
+    subprocess.run(command, capture_output=True, text=True)
+
     st.title("Product Scraper")
     urls = st.text_input("Enter a Website URLs: ")
 
@@ -16,15 +27,10 @@ def main():
         body_content = parser.body_content(result)
         cleaned_content = parser.cleaned_content(body_content)
 
-        #st.session_state.dom_content = result
-
-        #with st.expander("View"):
-            #st.text_area("DOM", cleaned_content, height=300)
-
         if result != "NO RESPONSE":
             dom_chunks = parser.split_dom_content(cleaned_content)
             result = llama_model.parse_ollama(dom_chunks)
         st.write(result)
-    
+
 if __name__ == "__main__":
     main()
